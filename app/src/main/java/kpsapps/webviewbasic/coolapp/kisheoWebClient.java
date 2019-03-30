fe707af9b4c67b6a9a10ceaf5c46b90434d8f44b
KPS (Kisheo Productions Studio) <37780674+Kisheo@users.noreply.github.com>
2019-03-30 05:43:17 -0300

    Update kisheoWebClient.java

--- app/src/main/java/kpsapps/webviewbasic/coolapp/kisheoWebClient.java
+++ app/src/main/java/kpsapps/webviewbasic/coolapp/kisheoWebClient.java
@@ -1,1 +1,8 @@
+package kpsapps.webviewbasic.coolapp;
+
 import com.kpsapps.sr.customview
+import android.app.DownloadManager;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
